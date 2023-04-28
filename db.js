fb3f6e992c15893593c7a3b46989580294f25ef1
vovaaxeapolla <vovaaxeapolla@gmail.com>
2023-04-28 23:20:50 +0500

    DB env

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    host: 'localhost',
+    host: process.env.DB_HOST,
     port: 5432,
-    user: 'postgres',
-    password: 'root',
-    database: 'sn'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 module.exports = pool;
