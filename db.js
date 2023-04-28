4e0d3d18a6489db038bfbcb5234fd7b605758c7a
vovaaxeapolla <vovaaxeapolla@gmail.com>
2023-04-29 00:29:23 +0500

    DB env

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const pool = new Pool({
     host: process.env.DB_HOST,
-    port: 5432,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
--- middlewares/cors-middleware.js
+++
@@ -1,6 +0,0 @@
-module.exports = function cors(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-}
